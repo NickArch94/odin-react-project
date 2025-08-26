85af5e164a17a85ec981354102f4cb31fc9e1155
NickArch94 <158612778+NickArch94@users.noreply.github.com>
2025-08-26 12:47:52 -0600

    Added buttons to 'ButterySmoothLists' to showcase using functions as props.

--- myodin-react-app/src/ButterySmoothLists.jsx
+++ myodin-react-app/src/ButterySmoothLists.jsx
@@ -16,6 +16,17 @@ function List(props) {
     )
 }
 
+let Button = ({text = "Click Here to Win!", color = "red", fontSize = 34, handleClick }) => {
+    const buttonStyle = {
+        color: color,
+        fontSize: fontSize + 'px',
+    }
+
+    return (
+        <button onClick={handleClick} style={buttonStyle}>{text}</button>
+    )
+
+}
 
 function ButterySmoothLists() {
     const smoothness = [
@@ -23,10 +34,16 @@ function ButterySmoothLists() {
         {id: 3, name: "Einstein Bros Shmear", smoothness: 'Super Smooth'},
         {id: 4, name: 'operator', smoothness: 'Smooth'}]
 
+    const handleButtonClick = () => {
+        window.location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+    }
+
     return (
         <div>
             <h1>Smoothness List:</h1>
                 <List smoothness={smoothness} />
+            <Button handleClick={handleButtonClick}/>
+            <Button text="Don't click me! I'm malware (very scary, very evil)" handleClick={() => alert("You have been hacked! Just kidding, but seriously don't click random buttons on the internet.")} color="green" fontSize={16} />
         </div>
     )
   }
