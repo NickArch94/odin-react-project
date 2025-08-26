88157faa50466cba32c1f9d1c40caa2fdd3e0807
NickArch94 <158612778+NickArch94@users.noreply.github.com>
2025-08-25 20:58:06 -0600

    Added some more logic to my 'ButterySmoothLists' component.

--- myodin-react-app/src/ButterySmoothLists.jsx
+++ myodin-react-app/src/ButterySmoothLists.jsx
@@ -1,30 +1,32 @@
 function List(props) {
     return (
         <>
-            {!props.smoothness && <div>Loading...</div>}
-            {props.smoothness && props.smoothness.length > 0 && (
+            {!props.smoothness ? (
+                <div>Loading...</div>
+            ) : props.smoothness && props.smoothness.length > 0 ? (
                 <ul>
-                    {props.smoothnness.map((smoothItem) => {
+                    {props.smoothness.map((smoothItem) => {
                         return <li key={smoothItem.id}>{smoothItem.name} - {smoothItem.smoothness}</li>
                     })}
                 </ul>
+            ) : (
+            <div>No items found.</div>
             )}
-            {props.smoothness && props.smoothness.length === 0 && <div>No items found.</div>}
         </>
     )
 }
 
 
 function ButterySmoothLists() {
-    let smoothness = [
+    const smoothness = [
         {id: 2, name: 'Marble Flooring', smoothness: 'Extremely Smooth'},
         {id: 3, name: "Einstein Bros Shmear", smoothness: 'Super Smooth'},
-        {id: 4, name: 'operator', smoothness: 'Smooth'},]
+        {id: 4, name: 'operator', smoothness: 'Smooth'}]
 
     return (
         <div>
             <h1>Smoothness List:</h1>
-                <List smoothList={smoothness} />
+                <List smoothness={smoothness} />
         </div>
     )
   }
